9062865703634ea34c61f6aaaa910621d807fab8
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-08-20 13:11:42 -0400

    primera funcionalidad: ingresar y mostrar cantidad de items

--- src/presenter.js
+++ src/presenter.js
@@ -2,12 +2,10 @@ import sumar from "./sumador";
 import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 const firstMul = document.querySelector("#primer-numeroMul");
-const secondMul = document.querySelector("#segundo-numeroMul");
 const formMul =document.querySelector("#multiplicar-form");
 const divMul= document.querySelector("#resultado-divMul");
 
@@ -15,16 +13,14 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" +"La cantidad de Items es: "+firstNumber + "</p>";
 });
 
 formMul.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumberMul = Number.parseInt(firstMul.value);
-  const secondNumberMul = Number.parseInt(secondMul.value);
 
-  divMul.innerHTML = "<p>" + multiplicar(firstNumberMul, secondNumberMul) + "</p>";
+  divMul.innerHTML = "<p>" +"El precio por item es: "+ firstNumberMul + "</p>";
 });
